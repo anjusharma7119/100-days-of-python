49dc376354aa2ac408548e9524afd29453557582
anjusharma7119 <121109927+anjusharma7119@users.noreply.github.com>
2024-06-12 15:11:33 +0530

    Create 1day.py

---
+++ 1day.py
@@ -0,0 +1,46 @@
+# print("hello world")
+# print(3+2)
+
+# #print multiple variables using comma and + operator
+# x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x,y,z)
+# print(x + y + z)
+
+# a = 5
+# print(x,a)
+# """ print(x+a)
+# this will give error"""
+
+
+# name = input("enter your name: ")
+
+# #string concatenation
+# print("your name is " + name)
+
+# #f string
+# print(f"your name is {name}")
+
+# #length function
+# l = len(name)
+# #we have to use str function to convert int l to str l
+# print("your name has "+str(l)+" characters")
+
+# #type function
+# print(type(name))
+# print(type(l))
+
+# #input function return a string
+# num = input("enter a number: ")
+# print(type(num))
+
+#project
+
+print("Welcome to The Band Name Generator!")
+city = input("What city did you grow up:\n")
+petName = input("What is your pet name:\n")
+
+print("Your band name is " + city + " " + petName)
+
+
